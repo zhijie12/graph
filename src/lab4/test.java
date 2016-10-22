6c6b8922ab7774dc626e4a0968db9b372ffd6f9e
Oh Zhi Jie <zhijie12@gmail.com>
2016-10-22 13:43:36 +0800

    add some small stuffs.

--- src/lab4/test.java
+++ src/lab4/test.java
@@ -12,12 +12,27 @@ package lab4;
 public class test {
     
     public static void main(String[] args) {
-        Graph g = new Graph(10,10);
+        
+        Graph g = new Graph(10,20); //NumOfVertices, NumOfEdges
     
-        System.out.println("a");
+        System.out.println("----------");
         g.breathFirstSearch(g.getVertex(0));
+        System.out.println("----------");
         g.breathFirstSearch(new Vertex(1));
         
+        /*
+        Graph g1 = new Graph(5000, 1000);
+        Graph g2 = new Graph(5000, 5000);
+        Graph g3 = new Graph(5000, 10000);
+        Graph g4 = new Graph(5000, 50000);
+        Graph g5 = new Graph(5000, 100000);
+
+        Graph g6 = new Graph(10000, 1000);
+        Graph g7 = new Graph(10000, 5000);
+        Graph g8 = new Graph(10000, 10000);
+        Graph g9 = new Graph(10000, 50000);
+        Graph g10 = new Graph(10000, 100000);        
+        */
     }
     
 }
